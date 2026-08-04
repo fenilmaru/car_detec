7ea083d9d65778c41bb7909f869e6cc2de09f08f
Fenil Maru <fenilh1323@gmail.com>
2026-08-04 15:24:50 +0530

    Resolve remaining merge conflicts in views.py files

--- manage.py
+++ manage.py
@@ -3,17 +3,9 @@
 import os
 import sys
 
-from pathlib import Path
-
-BASE_DIR = Path(__file__).resolve().parent
-sys.path.insert(0, str(BASE_DIR / "autonomous_activation_system"))
-
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault(
-        "DJANGO_SETTINGS_MODULE",
-        "autonomous_activation_system.config.settings"
-    )
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
@@ -26,4 +18,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-
